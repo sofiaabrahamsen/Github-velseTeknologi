549139af0384603540334b795523defade49674f
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2022-09-27 08:52:24 +0200

    hej, det virker :D

--- src/main/java/com/example/githubactionovelsetek/MyController.java
+++ src/main/java/com/example/githubactionovelsetek/MyController.java
@@ -1,7 +1,8 @@
 package com.example.githubactionovelsetek;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class MyController {
     @GetMapping("/")
     public String index(){
